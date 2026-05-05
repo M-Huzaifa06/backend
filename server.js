fc33a682a07283d05bdfb4d5d76499b96dd59a2b
M-Huzaifa06 <mughalhuzaifa3486@gmail.com>
2026-05-04 22:40:40 -0700

    server commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 
 const allowedOrigins = [
-  'https://barber-shop-nine-mu.vercel.app',
+  'https://barber-shop-omega-nine.vercel.app',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000',
